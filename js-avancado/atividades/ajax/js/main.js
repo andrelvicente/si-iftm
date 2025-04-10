58aa9b7c685a9d8cf693aaf92032cfedd07c4ce7
André Luiz Vicente Silva <andre.silva@estudante.iftm.edu.br>
2025-04-10 20:29:08 -0300

    feat: update github url

--- js-avancado/atividades/ajax/js/main.js
+++ js-avancado/atividades/ajax/js/main.js
@@ -1,7 +1,7 @@
 import { carregarComFetch } from './fetchMethod.js';
 import { carregarComXHR } from './xhrMethod.js';
 
-const url = 'https://raw.githsercontent.com/andrelvicente/si-iftm/main/js-avancado/atividades/ajax/dados.json';
+const url = 'https://raw.githubusercontent.com/andrelvicente/si-iftm/main/js-avancado/atividades/ajax/dados.json';
 
 carregarComFetch(url);
 carregarComXHR(url);
