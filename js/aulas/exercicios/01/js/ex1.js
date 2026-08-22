a3ab03b5f905dbd425b262a7e7997fbdb8f70a67
André Luiz Vicente Silva <andrevicente.ufu@gmail.com>
2026-08-21 21:50:18 -0300

    fix: update prompt default value and adjust initials extraction condition to include two-character parts

--- js/aulas/exercicios/01/js/ex1.js
+++ js/aulas/exercicios/01/js/ex1.js
@@ -1,9 +1,10 @@
-var nomeCompleto = prompt("Informe o seu nome completo:");
+var nomeCompleto = prompt("Informe o seu nome completo:", "INSTITUTO FEDERAL DE SÃO PAULO");
 var partes = nomeCompleto.split(" ");
 var iniciais = "";
 
 for (var i = 0; i < partes.length; i++) {
-    if (partes[i].length < 2) {
+    console.log(partes[i].length);
+    if (partes[i].length <= 2) {
         continue;
     }
     iniciais += partes[i].charAt(0);
